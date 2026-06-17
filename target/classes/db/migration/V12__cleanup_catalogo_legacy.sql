981e7b84096777961198b47544ba2a83dec106aa
Alfonso Gonzalez <agonzalez@demafront.org>
2026-06-17 19:57:22 -0400

    se incluye .gitignore del ms, ademas de eliminar elementos asociados a este

--- target/classes/db/migration/V12__cleanup_catalogo_legacy.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE catalogo_items DROP COLUMN categoria;
-
-ALTER TABLE movimientos_inventario ALTER COLUMN item_catalogo_id SET NOT NULL;
--- target/classes/db/migration/V4__create_eventos_procesados.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE eventos_procesados (
-    evento_id       UUID PRIMARY KEY,
-    tipo_evento     VARCHAR(100) NOT NULL,
-    procesado_en    TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
-CREATE INDEX idx_eventos_procesados_tipo ON eventos_procesados (tipo_evento);
